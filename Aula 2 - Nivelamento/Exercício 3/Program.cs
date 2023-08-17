0aec631d0641019f82d0b4bbc467f4759f645a9b
ANA LUISA CAPATO <134103817+AnaLuisaCapato@users.noreply.github.com>
2023-08-17 16:04:34 -0300

    Exercícío 3 OK

--- Aula 2 - Nivelamento/Exercício 3/Program.cs
+++ Aula 2 - Nivelamento/Exercício 3/Program.cs
@@ -1,13 +1,28 @@
-﻿using System;
-
-
+using System;
 namespace Aula2
 {
     class Program
     {
         static void Main(string[] args)
         {
-            
+            Console.Write("Digite a quantidade de produto em estoque:");
+            int qntdeAtual = int.Parse(Console.ReadLine());
+            Console.Write("Digite a capacidade máxima de produto em estoque:");
+            int qntdeMax = int.Parse(Console.ReadLine());
+            Console.Write("Digite a capacidade mínima de produto em estoque:");
+            int qntdeMin = int.Parse(Console.ReadLine());
+
+            int qntdeMedia = (qntdeMax + qntdeMin) / 2;
+
+            if (qntdeAtual >= qntdeMedia)
+            {
+                Console.WriteLine("Não efetuar compra!");
+            }
+            else
+            {
+                Console.WriteLine("Efetuar compra!!");
+            }
+            Console.ReadKey();
         }
     }
 }
