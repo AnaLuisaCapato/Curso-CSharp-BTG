cbd3fdff8c1bd99fbb35781a102e2624b33ea4f6
ANA LUISA CAPATO <134103817+AnaLuisaCapato@users.noreply.github.com>
2023-08-17 16:05:48 -0300

    Exercícío 4 OK

--- Aula 2 - Nivelamento/Exercício 4/Program.cs
+++ Aula 2 - Nivelamento/Exercício 4/Program.cs
@@ -1,5 +1,4 @@
-﻿using System;
-
+using System;
 namespace Aula2
 {
     class Program
@@ -7,6 +6,26 @@ namespace Aula2
         static void Main(string[] args)
         {
 
+            int[] vetor = new int[100];
+
+            Random random = new Random();
+
+            for (int i = 0; i < vetor.Length; i++)
+            {
+                vetor[i] = random.Next();
+            }
+
+            int maiorValor = vetor.Max();
+            int menorValor = vetor.Min();
+
+            Console.WriteLine("O maior valor é o " + maiorValor 
+                + " que está na posição:" 
+                + Array.IndexOf(vetor, maiorValor));
+            Console.WriteLine("O menor valor é o " + menorValor
+                + " que está na posição:" 
+                + Array.IndexOf(vetor, menorValor));
+
+            Console.ReadKey();
         }
     }
 }
